fbb7e478868ece04ad97aaf2a2e09c22b0aead11
Rakesh kumar <dr.rakesh684@gmail.com>
2022-05-01 20:31:30 +0530

    added some javascript function

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard + 4
+let secondCard = 10
+let sum = firstCard + secondCard 
 let hasBlackJack = false
 let isAlive = true
 let message=""
-
-
-if (sum <= 20) {
-    message="Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message="Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message="You're out of the game! 😭"
-    isAlive = false
+let messageEl =document.querySelector("#message-el");
+let sumEl=document.querySelector("#sum-el");
+let cardsEl=document.querySelector("#cards-el")
+let newButton=document.querySelector("new-button")
+function startGame(){
+    renderGame()
+    
 }
-console.log(message);
+function renderGame(){
+    cardsEl.textContent="Card : "+ firstCard +" "+secondCard
+    sumEl.textContent="Sum: "+ sum 
+    if (sum <= 20) {
+        message="Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message="You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message="You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent=message;
+}
+
+function newCard(){
+    let card=6
+    sum+=card;
+    renderGame()
+    cardsEl.textContent+=" " +card
+
+}
