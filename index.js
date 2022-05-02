632bfa6e0e84a3fb9ab52c44adb24eb250e71288
Rakesh kumar <dr.rakesh684@gmail.com>
2022-05-02 23:32:26 +0530

    added javascript

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
-let firstCard = 10
-let secondCard = 10
-let sum = firstCard + secondCard 
+let cards=[]
+let sum =0
+
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message=""
 let messageEl =document.querySelector("#message-el");
 let sumEl=document.querySelector("#sum-el");
 let cardsEl=document.querySelector("#cards-el")
 let newButton=document.querySelector("new-button")
+function getRandomCard(){
+    let randomNumber=Math.floor(Math.random()*13+1)
+    if(randomNumber===1){
+        return 11
+    }
+    else if(randomNumber>10){
+        return 10
+    }
+    else{
+        return randomNumber;
+    }
+     
+}
+
+
 function startGame(){
+    isAlive=true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards=[firstCard,secondCard]
+    sum=firstCard+secondCard
     renderGame()
     
 }
 function renderGame(){
-    cardsEl.textContent="Card : "+ firstCard +" "+secondCard
+    cardsEl.textContent="Card : "
+    for(let i=0;i<cards.length;i++){
+        cardsEl.textContent+=cards[i]+" "
+    }
     sumEl.textContent="Sum: "+ sum 
     if (sum <= 20) {
         message="Do you want to draw a new card?"
@@ -28,9 +51,9 @@ function renderGame(){
 }
 
 function newCard(){
-    let card=6
+    let card=getRandomCard();
     sum+=card;
+    cards.push(card);
     renderGame()
-    cardsEl.textContent+=" " +card
-
+    
 }
